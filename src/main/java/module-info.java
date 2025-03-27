3d1e1a1aee6521225dd8e694be5c474dfa95aa70
Theo Enfedaque <t.enfedaque@lprs.fr>
2025-03-27 18:46:14 +0100

    feat : Mpd oublié envoie d'email WIP (TP 5)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module appli {
     requires javafx.fxml;
     requires java.sql;
     requires spring.security.crypto;
+    requires javax.mail.api;
 
     opens appli to javafx.fxml;
     exports appli;
--- src/main/java/service/EmailService.java
+++ src/main/java/service/EmailService.java
@@ -1,4 +1,40 @@
 package service;
 
+import java.security.SecureRandom;
+import java.util.Properties;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 public class EmailService {
+    private static final SecureRandom random = new SecureRandom();
+    private static final String EMAIL_SENDER = "mpd.oublie@hoziodev.fr";
+    private static final String PASSWORD = "r53f!MEs";
+    public static void envoyerEmail(String destinataire, String sujet, String messageTexte) {
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", "smtp.hoziodev.fr");
+        properties.put("mail.smtp.port", "587");
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(EMAIL_SENDER, PASSWORD);
+            }
+        });
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(EMAIL_SENDER));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(destinataire));
+            message.setSubject(sujet);
+            message.setText(messageTexte);
+            Transport.send(message);
+            System.out.println("E-mail envoyé avec succès !");
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+    public static String genererCode() {
+        int code = 100000 + random.nextInt(900000);
+        return String.valueOf(code);
+    }
 }
